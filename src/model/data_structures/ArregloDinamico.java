f1c64078e2a4dd83f604d508100b24fb21850157
cpenuela20 <ce.penuela20@uniandes.edu.co>
2020-01-30 21:31:53 -0500

    buenas

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -7,71 +7,102 @@ package model.data_structures;
  * @author Fernando De la Rosa
  *
  */
-public class ArregloDinamico implements IArregloDinamico {
-		/**
-		 * Capacidad maxima del arreglo
-		 */
-        private int tamanoMax;
-		/**
-		 * Numero de elementos presentes en el arreglo (de forma compacta desde la posicion 0)
-		 */
-        private int tamanoAct;
-        /**
-         * Arreglo de elementos de tamaNo maximo
-         */
-        private String elementos[ ];
+public class ArregloDinamico<T extends Comparable<T>> implements IArregloDinamico<T> {
+	/**
+	 * Capacidad maxima del arreglo
+	 */
+	private int tamanoMax;
+	/**
+	 * Numero de elementos presentes en el arreglo (de forma compacta desde la posicion 0)
+	 */
+	private int tamanoAct;
+	/**
+	 * Arreglo de elementos de tamaNo maximo
+	 */
+	private T [ ] elementos;
 
-        /**
-         * Construir un arreglo con la capacidad maxima inicial.
-         * @param max Capacidad maxima inicial
-         */
-		public ArregloDinamico( int max )
-        {
-               elementos = new String[max];
-               tamanoMax = max;
-               tamanoAct = 0;
-        }
-        
-		public void agregar( String dato )
-        {
-               if ( tamanoAct == tamanoMax )
-               {  // caso de arreglo lleno (aumentar tamaNo)
-                    tamanoMax = 2 * tamanoMax;
-                    String [ ] copia = elementos;
-                    elementos = new String[tamanoMax];
-                    for ( int i = 0; i < tamanoAct; i++)
-                    {
-                     	 elementos[i] = copia[i];
-                    } 
-            	    System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
-               }	
-               elementos[tamanoAct] = dato;
-               tamanoAct++;
-       }
+	/**
+	 * Construir un arreglo con la capacidad maxima inicial.
+	 * @param max Capacidad maxima inicial
+	 */
+	public ArregloDinamico( int max )
+	{
 
-		public int darCapacidad() {
-			return tamanoMax;
+		elementos =  (T[]) new Object[max];
+		tamanoMax = max;
+		tamanoAct = 0;
+
+	}
+
+	public void agregar( T dato )
+	{
+		if ( tamanoAct == tamanoMax )
+		{  // caso de arreglo lleno (aumentar tamaNo)
+			tamanoMax = 2 * tamanoMax;
+			T [ ] copia = elementos;
+			elementos = (T[]) new Object [tamanoMax];
+			for ( int i = 0; i < tamanoAct; i++)
+			{
+				elementos[i] = copia[i];
+			} 
+			System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
+		}	
+		elementos[tamanoAct] =  dato;
+		tamanoAct++;
+	}
+
+	public int darCapacidad() {
+		return tamanoMax;
+	}
+
+	public int darTamano() {
+		return tamanoAct;
+	}
+
+	public T darElemento(int i) {
+
+		if ( i >=0 && i<elementos.length)
+		{
+			return elementos[i];
 		}
+		return null;
+	}
 
-		public int darTamano() {
-			return tamanoAct;
-		}
+	public T buscar(T dato) {
 
-		public String darElemento(int i) {
-			// TODO implementar
-			return null;
-		}
+		T retorno= null;
 
-		public String buscar(String dato) {
-			// TODO implementar
-			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-			return null;
+		for (int i = 0; i < elementos.length; i++) 
+		{
+			if(elementos[i].compareTo(dato)==0)
+			{
+				retorno = elementos[i];
+				break;
+			}
 		}
+		return retorno;
 
-		public String eliminar(String dato) {
-			// TODO implementar
-			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-			return null;
+
+		// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
+
+	}
+
+	public T eliminar(T dato) 
+	{
+		T retorno= null;
+
+		for (int i = 0; i < elementos.length; i++) 
+		{
+			if(elementos[i].compareTo(dato)==0)
+			{
+				retorno = elementos[i];
+				elementos[i]=null;
+				break;
+			}
 		}
+		return retorno;
+	}
+
+
 
 }
---
+++ src/model/data_structures/Nodo.java
@@ -0,0 +1,6 @@
+package model.data_structures;
+
+// Clase donde se almacenará la información de los elementos (genéricos) de la lista.
+public class Nodo {
+
+}
